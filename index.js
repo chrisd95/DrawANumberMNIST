cd81d27eb3b00f2eb6cdcf4ff4388015590df618
chrisd95 <christopher.dip@umontreal.ca>
2021-01-03 15:38:26 -0500

    Disabling scrolling for iOS devices

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/my-model.onnx',(req, res) => res.sendfile('views/pages/onnx_model.onnx'))
   .get('/canvas.js',(req, res) => res.sendfile('views/pages/canvas.js'))
-  .get('/scroll-lock.js',(req, res) => res.sendfile('views/pages/scroll-lock.js'))
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
