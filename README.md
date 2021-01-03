ab5631771e82c388c3e3519f8ca22ec039b6d449
chrisd95 <christopher.dip@umontreal.ca>
2021-01-02 23:41:20 -0500

    functional version

---
+++ README.md
@@ -0,0 +1,5 @@
+# 3DCheckerBoardCube
+p5js rendition of a 3d checkerboard patterned cube
+
+Live mode:
+https://fathomless-mountain-95073.herokuapp.com/
---
+++ views/pages/canvas.js
@@ -0,0 +1,121 @@
+const CANVAS_SIZE = 280;
+const CANVAS_SCALE = 0.5;
+
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const clearButton = document.getElementById("clear-button")
+
+
+let isMouseDown = false;
+let hasIntroText = true;
+let lastX = 0;
+let lastY = 0
+
+const sess = new onnx.InferenceSession()
+const loadingModelPromise = sess.loadModel('./my-model.onnx')
+
+ctx.lineWidth = 28;
+ctx.lineJoin = "round";
+ctx.font = "28px sans-serif";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+ctx.fillStyle = "#212121";
+
+ctx.fillText("Loading...", CANVAS_SIZE/2, CANVAS_SIZE/2)
+
+function clearCanvas() {
+  ctx.clearRect(0,0, CANVAS_SIZE,CANVAS_SIZE);
+  for (let i =0; i<10; i++){
+    const element = document.getElementById(`prediction-${i}`);
+    element.className = "prediction-col"
+    element.children[0].children[0].style.height = "0"
+  }
+}
+
+
+function drawLine(fromX, fromY, toX, toY){
+  ctx.beginPath();
+  ctx.moveTo(fromX, fromY);
+ctx.lineTo(toX, toY);
+ctx.closePath();
+ctx.stroke();
+updatePredictions();
+}
+
+
+async function updatePredictions(){
+  const imgData = ctx.getImageData(0,0, CANVAS_SIZE,CANVAS_SIZE)
+  const inputTwo = new onnx.Tensor(new Float32Array(imgData.data), "float32");
+  const input = new onnx.Tensor(new Float32Array(280*280*4), 'float32',[313600])
+  // console.log(inputTwo)
+  const outputMap = await sess.run([inputTwo]);
+
+  console.log(outputMap)
+  const outputTensor = outputMap.values().next().value;
+  const predictions = outputTensor.data
+  const maxPrediction = Math.max(...predictions);
+
+  for(let i = 0; i< predictions.length; i++){
+    const element = document.getElementById(`prediction-${i}`);
+
+    element.children[0].children[0].style.height = `${predictions[i] * 100}%`;
+    element.className =
+      predictions[i] === maxPrediction
+        ? "prediction-col top-prediction"
+        : "prediction-col";
+  }
+}
+
+function canvasMouseDown(event){
+  isMouseDown = true;
+  if (hasIntroText){
+    clearCanvas();
+    hasIntroText = false
+  }
+
+  const x = event.offsetX/CANVAS_SCALE
+  const y = event.offsetY/ CANVAS_SCALE
+
+  lastX = x + 0.001
+  lastY = y + 0.001
+  canvasMouseMove(event);
+}
+
+function canvasMouseMove(event){
+  const x = event.offsetX/CANVAS_SCALE
+  const y = event.offsetY/CANVAS_SCALE
+
+  if (isMouseDown) {
+    drawLine(lastX, lastY, x,y);
+  }
+
+  lastX = x;
+  lastY = y;
+}
+
+
+function bodyMouseUp(){
+  isMouseDown = false;
+}
+
+
+function  bodyMouseOut(event){
+  // We won't be able to detect a MouseUp event if the mouse has move
+  // Outside the window, so when the mouse leaves the window,
+  // we set ismousedown to false
+  if(!event.relatedTarget || event.relatedTarget.nodeName === "HTML"){
+    isMouseDown = false
+  }
+}
+
+loadingModelPromise.then(() =>{
+  canvas.addEventListener("mousedown", canvasMouseDown)
+  canvas.addEventListener("mousemove", canvasMouseMove)
+  document.body.addEventListener("mouseup", bodyMouseUp)
+  document.body.addEventListener("mouseout", bodyMouseOut)
+  clearButton.addEventListener("mousedown", clearCanvas)
+
+  ctx.clearRect(0,0, CANVAS_SIZE, CANVAS_SIZE)
+  ctx.fillText("Draw a number here!", CANVAS_SIZE/2, CANVAS_SIZE/2)
+})
